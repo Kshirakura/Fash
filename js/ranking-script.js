944fed49147f28aa322a650a56c5b3aa9a233586
Kshirakura <49435807+Kshirakura@users.noreply.github.com>
2021-07-30 13:05:19 +0900

    Add files via upload

---
+++ js/ranking-script.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.color-btn').click(function(){
+    $('.picked').removeClass('picked');
+    $('.active').removeClass('active');
+    $(this).addClass('picked');
+    var index = $('.color-btn').index(this);
+    $('.item').eq(index).addClass('active');
+  });
+
+  $('.number').change(function() {
+    var value = 10000;
+      var subsum = value * parseInt($('.number').children('input').val());
+      $('.subtotal').html('￥' + subsum.toLocaleString());
+  });
+  
+});
--- js/script.js
+++ js/script.js
@@ -2,4 +2,18 @@ $(function(){
 
   $('.slide-show').slick();
 
+  $('.menu-btn').click(function(){
+    if($('.modal').css('display') == 'block') {
+      $('.modal').slideUp();
+    } else {
+      $('.modal').slideDown();
+    }
+  });
+
+  $('.top-btn').click(function() {
+    $('html, body').animate({
+      scrollTop:0
+    }, 400);
+  });
+
 });
